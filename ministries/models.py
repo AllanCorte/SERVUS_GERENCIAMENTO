16a9c88dceaaf4114b47c6c6b49c1092cd917e9c
AllanCorte <allanalvescorte@gmail.com>
2026-03-14 23:00:05 -0400

    criando models ministries

--- ministries/models.py
+++ ministries/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from people.models import Pessoa
 # Create your models here.
+
+
+class Ministerio(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = 'Ministério'
+        verbose_name_plural = 'Ministerios'
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
+
+
+class MembroMministerio(models.Model):
+    ministerio = models.Foreignkey(
+        Ministerio, on_delete=models.CASCADE, related_name='membros')
+    pessoa = models.Foreignkey(
+        Pessoa, on_delete=models.CASCADE, related_name='monisterio')
+    ativo = models.Booleanfield(default=True)
+
+    class Meta
+    verbose_name = 'Membro do Ministério'
+    verbose_name_plural = 'Membros dos Ministérios'
+    unique_together = ['ministério', 'pessoa']
+
+    def __str__(self):
+        return f'{self.pessoa.nome} - {self.ministerio.nome}'
